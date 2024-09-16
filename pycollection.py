1de03e5a11e0d0aa76275201d1512fa78435a992
Mustapha Oluwasarasimi <mustaphaoluwadarasimi5@gmail.com>
2024-09-16 12:56:30 +0100

    added pycollection.py

---
+++ pycollection.py
@@ -0,0 +1,22 @@
+"""
+list
+tuple
+set
+dictionary
+"""
+# Creating a list in python
+list_of_item= ['1', 3j, 6.8, True, ['Temi', 'Taiwo']]
+list_of_student= (['kiki', 'bolu', 'tope', ['dara', 'dray']])
+list_of_student1= (['kiki', 'bolu', 'tope', ['dara', 'dray'], 'kiki', 'bolu', 'tope'])
+list_of_student2= ([['kiki', 'bolu', 'tope'], ['dara', 'dray'], ['kiki', 'bolu', 'tope']])
+
+# print(type(list_of_item))
+# print(type(list_of_student))
+# print(type(list_of_student1))
+
+# print(len(list_of_student1))
+# print(len(list_of_student))
+# print(len(list_of_item))
+
+# print(list_of_student1[3][1])
+print(list_of_student2[0][1], list_of_student2[1][0])
