e1f6fb5fb3b021458195ef70b0334e20bff5e094
Mustapha Oluwasarasimi <mustaphaoluwadarasimi5@gmail.com>
2025-08-18 11:54:40 +0100

    Added a module for password

--- Database_/dbclass.py
+++ Database_/dbclass.py
@@ -1,6 +1,11 @@
 # pip install pymysql
 
+import os
 import pymysql as pyms
+from dotenv import load_dotenv
+load_dotenv()
 
-my_con = pyms.connect(host = '127.0.0.1', user = 'root', password = 'Musty@@@@@12345')
+dbPass = os.getenv("DB_PASS")
+
+my_con = pyms.connect(host = '127.0.0.1', user = 'root', password = dbPass)
 print('Connection Successful')
