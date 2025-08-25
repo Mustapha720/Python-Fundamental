9e0b336cfa28e3d181f7d8470c6941f0bc7e1e92
Mustapha Oluwasarasimi <mustaphaoluwadarasimi5@gmail.com>
2025-08-25 14:48:48 +0100

    ....

--- colors.py
+++ colors.py
@@ -1,7 +1,8 @@
-from colorama import init, Fore, Style
+from colorama import init, Fore, Style, Back
 
 init(autoreset = True)
 
-print(Fore.GREEN + "This is a green text!")
+print(Back.CYAN + "yo")
+print(Fore.GREEN + "This is a green text!" + Fore.RED + " YO")
 print(Style.BRIGHT + Fore.YELLOW + "Bright!")
 print("normal text")
