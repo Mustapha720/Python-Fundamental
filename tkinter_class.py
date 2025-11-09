d7f217eedcb46d74886c8b03b6d9947eef5df9f6
Mustapha Oluwasarasimi <mustaphaoluwadarasimi5@gmail.com>
2025-11-09 18:52:27 +0100

    Update label properties and reposition button in tkinter GUI

--- tkinter_class.py
+++ tkinter_class.py
@@ -9,7 +9,16 @@ window.title("Basic GUI")
 icon = PhotoImage(file = 'OIP.png')
 window.iconphoto(True, icon)
 window.config(background = "red")
-label = Label(window, text = 'Hello World!', font=('aerial', 20, 'bold'), fg='green', bg='cyan')
+photo = PhotoImage(file='')
+label = Label(window, text = 'Hello World!',
+            font=('aerial', 20, 'bold'),
+            fg='green',
+            bg='black',
+            relief= RAISED,
+            bd= 10,
+            padx= 20,
+            pady= 20
+            )
 # label.pack()
 label.place(x = 0, y = 1)
 button = Button(window, text='Click me!!!')
@@ -19,5 +28,5 @@ button.config(activebackground = "#FFF000")
 button.config(activeforeground = "#fffb1f")
 image = PhotoImage(file = "svgexport-28.png")
 button.config(image=image)
-button.place(x = 0, y = 42)
+button.place(x = 0, y = 92)
 window.mainloop()
